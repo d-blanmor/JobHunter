52c4cd1b2499bcf3a2e9c2bccb04c95b44330fd7
Daniel Blanco <daniel.blancomorales@gmail.com>
2026-07-06 17:08:55 +0100

    Update windows launcher

--- run_jobhunter.bat
+++ run_jobhunter.bat
@@ -4,4 +4,8 @@ set api_port="8000"
 set ui_port=""
 
 cd server
-.venv\Scripts\uvicorn app.main:app --host %host% --port %api_port%
+START .venv\Scripts\uvicorn app.main:app --host %host% --port %api_port%
+
+cd ..\client-web
+START npm run dev
+

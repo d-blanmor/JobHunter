5f0f2ae1363743a5e8e161891f83a8434ac58ede
Daniel Blanco <daniel.blancomorales@gmail.com>
2026-07-21 19:15:23 +0100

    config changes

--- run_jobhunter.bat
+++ run_jobhunter.bat
@@ -1,11 +1,11 @@
 @echo off
-set host="0.0.0.0"
+set api_host="0.0.0.0"
 set api_port="8000"
-set ui_port=""
+set env="dev"
 
 cd server
-START .venv\Scripts\uvicorn app.main:app --host %host% --port %api_port%
+START .venv\Scripts\uvicorn app.main:app --host %api_host% --port %api_port%
 
 cd ..\client-web
-START npm run dev
+START npm run %env%
 

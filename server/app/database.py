0c8e219b59c0fd4fd3890e02239e2d8d8ff9b867
Daniel Blanco <daniel.blancomorales@gmail.com>
2026-07-27 01:53:11 +0100

    Adding initial settings for Ollama integration

--- server/app/database.py
+++ server/app/database.py
@@ -53,8 +53,22 @@ def init_db() -> None:
             session.add(benefit)
             session.flush()
 
-            setting = models.appSetting(Key="db_version", Value="1.0.0", Notes="Version control of the last deployed database", IsActive=True);
+            setting = models.appSetting(Key="db_version", Value="1.0.0", Notes="Version control of the last deployed database.", IsActive=True);
             session.add(setting)
+            setting = models.appSetting(Key="OLLAMA_URL", Value="http://localhost:11434", Notes="Ollama API listening URL.", IsActive=True);
+            session.flush()
+            setting = models.appSetting(Key="OLLAMA_API_KEY", Value="", Notes="Security API key for calling Ollama.", IsActive=True);
+            session.flush()
+            setting = models.appSetting(Key="OLLAMA_MODEL", Value="gemma4:latest", Notes="LLM model installed in Ollama.", IsActive=True);
+            session.flush()
+            setting = models.appSetting(Key="OLLAMA_SYSTEM_PROMPT", Value="You are an an expert career manager, your goal is to help users optimise their resumes for job applications. Follow these steps to provide effective guidance: \
+- When a job specification is provided, analyse the target job description and suggest relevant keywords to align the resume with the job requirements and increase chances of passing applicant tracking systems (ATS). Extract from it key points such as role, company, sector, skill set desired, technologies, and provide a honest opinion, highlighting any red flag you may notice. \
+- If a resume is also provided, match it with the job description, and add to the role analysis your honest opinion of the qualification of the user for the role, strengths, weaknesses, keep it realistic. Give honest advice to the user. As final point, provide in percentage, the match of the resume to the role. \
+- Keep your responses brief and concise, be realistic with your analysis, always with a professional tone, avoid using emojis or any other non professional expressions. \
+- You must provide citations to the job spec, resume, or any other source for your statements. \
+- Remember to keep personal information confidential and secure at all times.", Notes="System prompt for Ollama requests.", IsActive=True);
+            session.flush()
+            setting = models.appSetting(Key="OLLAMA_KNOWLEDGE_SOURCE", Value="", Notes="Full name of text file with the users resume to be used in queries.", IsActive=True);
             session.flush()
 
             session.commit()
